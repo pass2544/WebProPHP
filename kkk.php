bc36cccef52278b86517e1c54d18d9311dcb22c4
pass2544 <6206021620094@fitm.kmutnb.ac.th>
2021-01-27 09:00:38 +0700

    create kkk.php

---
+++ kkk.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset = "UTF-8">
+        
+        <title>index</title>
+    </head>
+    <body>
+         <?php
+            echo "<h1>hello KMUTNB Prachinburi</<h1><br>";
+            echo "<h2>Prachinburi<h2>";
+            ?>
+        
+    </body>
+</html>
